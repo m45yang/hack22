c573c3d2cee1e2aff2a7c50cdaf3dd3f2b7ce86d
Matthew Yang <myang@uwaterloo.ca>
2017-03-09 14:19:00 -0800

    game schema

---
+++ models/game.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var Player = new Schema({
+  user_id: String,
+  business_id: String,
+  state: {
+    type: String,
+    enum: ['alive', 'dead']
+  }
+  score: Number,
+});
+
+var Game = new Schema({
+  shortId: String,
+  state: {
+    type: String,
+    enum: ['waiting', 'playing', 'end']
+  }
+  players: [Player],
+}, { collection: 'games' });
+
+module.exports = mongoose.model('Game', Game);
